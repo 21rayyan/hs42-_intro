fc925d9d2652e8290a6d8031e5623359587bf71d
Rayyan Shaikh <rshaikh@e1z2r5p48.42.us.org>
2018-02-03 14:18:33 -0800

    im super lazy so my mentor is doing this for me

---
+++ talk/ex01.py
@@ -0,0 +1,13 @@
+def main():
+	var1 = raw_input("Write an adj: ")
+	var2 = raw_input("Write a buisness: ")
+	var3 = raw_input("Write a animal: ")
+	var4 = raw_input("Write a noise: ")
+
+	print(var1+" mcdonald had a "+var2)
+	print("and on that "+var2+" he had a" +var3)
+	print("with a "+var3+ var3+" here")
+	print("and a "+var4+ var4+" there")
+	print(var1+" Mcdonald had a " +var2+" EIEIO")
+
+main()
